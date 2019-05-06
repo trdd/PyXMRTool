956bce48d3f9f91e52f261e4ef10a45c5242a852
malaclypseII <yannic.utz@posteo.net>
2019-05-06 11:43:05 +0200

    solved iss16; added licence hint to Pythonreflectivity

--- Pythonreflectivity/MOKE_polar.pyx
+++ Pythonreflectivity/MOKE_polar.pyx
@@ -1,3 +1,20 @@
+#<Pythonreflectivityl: A Python Package for simulation of x-ray reflectivities of Heterostructures>
+#    Copyright (C) <2017>  <Martin Zwiebler>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 cdef void Fill_rMemory_z(rMemory *Mem, double vy, double vyvy, double omvyvy, double complex chix, double complex chiy, double complex chiz, double complex chig):
     cdef double complex xld, magrest, xldsquared, diftester, diftester2
 
--- Pythonreflectivity/MOKE_transversal.pyx
+++ Pythonreflectivity/MOKE_transversal.pyx
@@ -1,3 +1,18 @@
+#<Pythonreflectivityl: A Python Package for simulation of x-ray reflectivities of Heterostructures>
+#    Copyright (C) <2017>  <Martin Zwiebler>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
 cdef void Relevant_Stuff_for_xmag_precisely(double complex cy1, double complex cy2, double complex cz1, double complex cz2, \
